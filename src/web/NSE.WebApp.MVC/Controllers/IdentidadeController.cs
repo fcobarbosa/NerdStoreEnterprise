de61ba9fb65865d035ecfe532ee350ee1b1c3492
fcobarbosa <fcobarbosa2006@hotmail.com>
2021-08-11 21:51:55 -0300

    Controller e View de Usuário

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,12 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using NSE.WebApp.MVC.Models;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public IActionResult Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //API - Registro
+
+            if (false) return View(usuarioRegistro);
+
+            //realizar login na APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(UsuarioLogin usuarioLogin, string returnUrl = null)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+            
+            //API - Login
+
+            if (false) return View(usuarioLogin);
+
+            //realizar login na APP
+            
+            return RedirectToAction(returnUrl);
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
