d2667b7fd12a02a8d6f537af4fd75944d3108974
fcobarbosa <fcobarbosa2006@hotmail.com>
2021-08-12 21:24:10 -0300

    Obtenção de informação do usuário

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 
 namespace NSE.WebApp.MVC.Configuration
@@ -8,6 +10,8 @@ namespace NSE.WebApp.MVC.Configuration
         public static void RegisteServices(this IServiceCollection services)
         {
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUser, AspNetUser>();
         }
     }
 }
---
+++ src/web/NSE.WebApp.MVC/Extensions/AspNetUser.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class AspNetUser : IUser
+    {
+        private readonly IHttpContextAccessor _acessor;
+        public AspNetUser(IHttpContextAccessor acessor)
+        {
+            this._acessor = acessor;
+        }
+        public string Name => this._acessor.HttpContext.User.Identity.Name;
+
+        public bool EstaAutenticado()
+        {
+            return this._acessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public IEnumerable<Claim> ObterClaims()
+        {
+            return this._acessor.HttpContext.User.Claims;
+        }
+
+        public HttpContext ObterContext()
+        {
+            return this._acessor.HttpContext;
+        }
+
+        public string ObterUserEmail()
+        {
+            return EstaAutenticado() ? this._acessor.HttpContext.User.GetUserEmail() : string.Empty;
+        }
+
+        public Guid ObterUserId()
+        {
+            return EstaAutenticado() ? Guid.Parse( this._acessor.HttpContext.User.GetUserId()) : Guid.Empty;
+        }
+
+        public string ObterUserToken()
+        {
+            return EstaAutenticado() ? this._acessor.HttpContext.User.GetUserToken() : string.Empty;
+        }
+
+        public bool PossuiRole(string role)
+        {
+            return this._acessor.HttpContext.User.IsInRole(role);
+        }
+    }
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            return principal.GetClaim("sub");
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            return principal.GetClaim("email");
+        }
+
+        public static string GetUserToken(this ClaimsPrincipal principal)
+        {
+            return principal.GetClaim("JWT");
+        }
+
+        private static string GetClaim(this ClaimsPrincipal principal, string name)
+        {
+            if (principal == null) throw new ArgumentException(nameof(principal));
+            var claim = principal.FindFirst(type: name);
+            return claim?.Value;
+        }
+    }
+}
