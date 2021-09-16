a0d6ea1a79a15ed33bbcad1668d4da565cbe2cfe
fcobarbosa <fcobarbosa2006@hotmail.com>
2021-09-16 04:35:17 -0300

    Adicionado UseAuthConfiguration a Catálogo.API

--- src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
+++ src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.Catalogo.API.Data;
+using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Catalogo.API.Configuration
 {
@@ -41,6 +42,8 @@ namespace NSE.Catalogo.API.Configuration
             app.UseRouting();
 
             app.UseCors("Total");
+
+            app.UseAuthConfiguration();
 
             app.UseEndpoints(endpoints =>
             {
