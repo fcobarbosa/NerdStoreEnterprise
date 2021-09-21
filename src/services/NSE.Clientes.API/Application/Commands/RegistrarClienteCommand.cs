25546f80efb9361b7c0147a7ad4fea3ba04cee60
fcobarbosa <fcobarbosa2006@hotmail.com>
2021-09-21 19:43:41 -0300

    Adicionada validação do command do cliente

--- src/services/NSE.Clientes.API/Application/Commands/RegistrarClienteCommand.cs
+++ src/services/NSE.Clientes.API/Application/Commands/RegistrarClienteCommand.cs
@@ -1,4 +1,5 @@
-﻿using NSE.Core.Messages;
+﻿using FluentValidation;
+using NSE.Core.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,42 @@ namespace NSE.Clientes.API.Application.Commands
             this.Nome = nome;
             this.Email = email;
             this.Cpf = cpf;
-    }
+        }
+        public override bool IsValid()
+        {
+            this.ValidationResult = new RegistrarClienteValidation().Validate(this);
+            return this.ValidationResult.IsValid;
+        }
+
+        public class RegistrarClienteValidation : AbstractValidator<RegistrarClienteCommand>
+        {
+            public RegistrarClienteValidation()
+            {
+                RuleFor(c => c.Id)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do cliente inválido");
+
+                RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .WithMessage("O nome do cliente não foi informado");
+
+                RuleFor(c => c.Cpf)
+                    .Must(CpfIsValid)
+                    .WithMessage("O CPF informado não é válido");
+
+                RuleFor(c => c.Email)
+                    .Must(EmailIsValid)
+                    .WithMessage("O e-mail informado não é válido");
+            }
+
+            protected static bool CpfIsValid(string cpf)
+            {
+                return Core.DomainObjects.Cpf.Validar(cpf);
+            }
+            protected static bool EmailIsValid(string email)
+            {
+                return Core.DomainObjects.Email.Validar(email);
+            }
+        }
     }
 }
